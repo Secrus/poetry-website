697e159bb647c02843d2507f94f52a80f4aeb010
Sébastien Eustace <sebastien@eustace.io>
2021-06-07 13:53:41 +0200

    Add linting/formatting

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,25 @@
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production"
 
 class TailwindExtractor {
-    static extract(content) {
-        return content.match(/[A-Za-z0-9-_:\/]+/g) || []
-    }
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+  }
 }
 
-
 module.exports = {
-    plugins: [
-        require("postcss-import"),
-        require("tailwindcss"),
-        require("autoprefixer"),
-        require("postcss-nested"),
-        ...(isProduction ? [require("@fullhuman/postcss-purgecss")({
-            content: [
-                "./themes/**/*.html",
-            ],
-            defaultExtractor: content => content.match(/[\w-:/]+(?<!:)/g) || []
-        })] : []),
-    ],
-};
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("postcss-nested"),
+    ...(isProduction
+      ? [
+          require("@fullhuman/postcss-purgecss")({
+            content: ["./themes/**/*.html"],
+            defaultExtractor: (content) =>
+              content.match(/[\w-:/]+(?<!:)/g) || [],
+          }),
+        ]
+      : []),
+  ],
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,34 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors")
 
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   purge: {
-    content: ["./themes/**/*.html"]
+    content: ["./themes/**/*.html"],
   },
-  darkMode: 'media',
+  darkMode: "media",
   theme: {
     fontFamily: {
-      'mono': 'Hack, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-      'sans': '"Inter var", -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Ubuntu,sans-serif',
-      'subtitle': 'Montserrat, sans-serif'
+      mono: 'Hack, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+      sans: '"Inter var", -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Ubuntu,sans-serif',
+      subtitle: "Montserrat, sans-serif",
     },
     extend: {
       colors: {
         blue: colors.lightBlue,
         cyan: colors.cyan,
-        primary: '#093D8D',
-        code: '#008ADD',
-        'light-primary': '#F0F4FC',
-        'light-note': '#F6F9FC',
-        'link': '#6772E5',
-        'link-hover': '#32325D'
+        primary: "#093D8D",
+        code: "#008ADD",
+        "light-primary": "#F0F4FC",
+        "light-note": "#F6F9FC",
+        link: "#6772E5",
+        "link-hover": "#32325D",
       },
       fontSize: {
-        'base': '0.9rem',
+        base: "0.9rem",
       },
       maxWidth: {
-        '11/12': '91.666667%'
-      }
+        "11/12": "91.666667%",
+      },
     },
   },
   variants: {
